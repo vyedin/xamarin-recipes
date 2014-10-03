742f68d62048ac7aa6c1774c1962f70151f1f1b2
travisjlewis <travis.lewis@xamarin.com>
2014-10-03 15:59:03 -0400

    Add scale droid project

---
+++ ScaleDroid/ScaleDroid/MainActivity.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+using Android.App;
+using Android.OS;
+using Android.Views.Animations;
+using Android.Widget;
+using Android.Animation;
+
+namespace ScaleDroid
+{
+	[Activity (Label = "ScaleDroid", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		bool isImageLarge = false;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+			ImageView imageToScale= FindViewById<ImageView> (Resource.Id.imageToScale);
+			Button scaleButton = FindViewById<Button> (Resource.Id.scaleButton);
+			ValueAnimator growAnimator = ValueAnimator.OfFloat(1.0f, 3.0f);
+			growAnimator.SetDuration(1000);
+			growAnimator.Update += (object sender, ValueAnimator.AnimatorUpdateEventArgs e) => {
+				imageToScale.ScaleX = (float) e.Animation.AnimatedValue;
+				imageToScale.ScaleY = (float) e.Animation.AnimatedValue;
+			};
+			// Get our button from the layout resource,
+			// and attach an event to it
+			
+			scaleButton.Click+= (object sender, EventArgs e) => {
+				if(isImageLarge) {
+					growAnimator.Reverse();
+					isImageLarge = false;
+					scaleButton.Text = "Grow Image!";
+				}
+				else {
+					growAnimator.Resume();
+					isImageLarge = true;
+					scaleButton.Text = "Shrink Image!";
+				}
+			};
+		}
+	}
+}
+
+
